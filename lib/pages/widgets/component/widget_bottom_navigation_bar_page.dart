e1ad3266fe46d548b5db37a8226f60680dd5b6b4
hefei <1349177631@qq.com>
2020-12-29 18:12:05 +0800

    widget-component: Drawer and BottomNavigationBar

--- lib/pages/widgets/component/widget_bottom_navigation_bar_page.dart
+++ lib/pages/widgets/component/widget_bottom_navigation_bar_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 ///
 /// author: hefei
 /// time  : 2020/12/28
-/// desc  :
+/// desc  : the example for BottomNavigationBar
+///         it's can not switch tab by slide
 ///
 class WidgetBottomNavigationBarPage extends StatefulWidget {
   @override
@@ -11,11 +12,47 @@ class WidgetBottomNavigationBarPage extends StatefulWidget {
 }
 
 class _WidgetBottomNavigationBarPageState extends State<WidgetBottomNavigationBarPage> {
+  int _selectedIndex = 0;
+  var _pageList;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageList = [
+      Center(
+        child: Text("Home"),
+      ),
+      Center(
+        child: Text("Business"),
+      ),
+      Center(
+        child: Text("School"),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("BottomNavigationBar"),
+        ),
+        body: _pageList[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.business), label: "Business"),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
+          ],
+          fixedColor: Theme.of(context).primaryColor,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ));
+  }
 
-    );
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
-
--- lib/pages/widgets/component/widget_drawer_page.dart
+++ lib/pages/widgets/component/widget_drawer_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 ///
 /// author: hefei
 /// time  : 2020/12/28
-/// desc  :
+/// desc  : the example for Drawer
+///         focus: you can push to new page or change body directly
 ///
 class WidgetDrawerPage extends StatefulWidget {
   @override
@@ -11,11 +12,105 @@ class WidgetDrawerPage extends StatefulWidget {
 }
 
 class _WidgetDrawerPageState extends State<WidgetDrawerPage> {
+  int _selectedIndex = 0;
+  var _pageList = [WidgetDrawerHomePage(), WidgetDrawerSettingPage(), WidgetDrawerAccountPage()];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Drawer"),
+      ),
+      body: _pageList[_selectedIndex],
+      drawer: Drawer(
+        child: Container(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text("DrawerHeader"),
+              ),
+              ListTile(
+                leading: new Icon(Icons.home),
+                title: new Text("Home"),
+                onTap: () {
+                  switchPage(0);
+                },
+              ),
+              ListTile(
+                leading: new Icon(Icons.settings),
+                title: new Text("Setting"),
+                onTap: () {
+                  switchPage(1);
+                },
+              ),
+              ListTile(
+                leading: new Icon(Icons.account_box),
+                title: new Text("Account"),
+                onTap: () {
+                  switchPage(2);
+                },
+              ),
+              AboutListTile(
+                child: Text("About"),
+                applicationName: "flutter_sample",
+                applicationVersion: "v1.0.0",
+                applicationIcon: Icon(Icons.adb),
+                icon: Icon(Icons.info),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void switchPage(int index) {
+    Navigator.of(context).pop();
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+class WidgetDrawerHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      color: Colors.lightGreen,
+      child: Center(
+        child: Text("Home"),
+      ),
     );
   }
 }
 
+class WidgetDrawerSettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amberAccent,
+      child: Center(
+        child: Text("Setting"),
+      ),
+    );
+  }
+}
+
+class WidgetDrawerAccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrangeAccent,
+      child: Center(
+        child: Text("Account"),
+      ),
+    );
+  }
+}
